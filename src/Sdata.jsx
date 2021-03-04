4187148d62f2342a9c70600a462313bacc4a59c9
Anshil Saxena <Anshil.Saxena@zimozi.co>
2021-03-04 15:33:14 +0530

    my third react website

---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        imgsrc: "https://i.mydramalist.com/XOZvg_4f.jpg",
+        title: "Availabe on Youtube",
+        sname: "My Unicorn Girl",
+        link: "https://www.youtube.com/watch?v=ei1ozR2CRFw&list=PLlCrV9TCfzMbBehy8VaniFf-4DfLIcDit",
+    },
+    {
+        imgsrc: "https://i.mydramalist.com/jgAAw_4f.jpg",
+        title: "Availabe on Youtube",
+        sname: "Perfect And Casual",
+        link: "https://www.youtube.com/watch?v=cmKhmQDqyVI&list=PLKetxu4L5G2B0U7MBX97npZl2qUmcsL1V",
+    },
+    {
+        imgsrc: "https://i.mydramalist.com/b2rxLf.jpg",
+        title: "Availabe on Youtube",
+        sname: "Love020",
+        link: "https://www.youtube.com/watch?v=T8oGhDZyPcA&list=PLNflQ0xt4CDZ7hsCUWvI7n6Rfnys1JDwx",
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Orignal series",
+        sname:"Dark",
+        link:"https://www.netflix.com/in/title/80990668?source=35",
+            },
+            {
+        imgsrc:"https://wallpapercave.com/wp/wp7418494.jpg",
+        title:"A Netflix Orignal series",
+        sname:"extra curricular",
+        link:"https://www.netflix.com/in/title/80990668?source=35",
+            },
+            {
+        imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Orignal series",
+        sname:"Stranger things",
+        link:"https://www.netflix.com/in/title/80990668?source=35",
+            },
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import Sdata from './Sdata';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  <>
+    <h1 className="heading_style"> My Favourite Series </h1>
+    {Sdata.map((val) => {
+      return (
+
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+
+      );
+    })}
+  </>
+
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
